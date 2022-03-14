8873a901157177448a31b5ebbf22593e556e887e
astepanov <57390938+andystep1@users.noreply.github.com>
2022-03-14 10:37:13 +0300

    add requirements

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ dp = Dispatcher(bot)
 async def start(message: types.Message):
     user_name = message.from_user.full_name
     user_id = message.from_user.id
-    await message.reply(f'Привет! {user_name}!')
+    await message.reply(f'Привет! {user_name}! Пришли мне фотографию бутылки и я скажу тебе что это за пиво')
 
 @dp.message_handler(content_types=['photo'])
 async def handle_docs_photo(message):
@@ -31,6 +31,13 @@ async def handle_docs_photo(message):
         answer = f'Вот что я нашел: \n<b>{name}</b> \n<b>Стиль</b>: {style} \n<b>Оценка</b>: {rating} из 5 \n<b>Крепость</b>: {ABV} \n<b>Плотность</b>: {IBU}'
         await message.answer(answer, parse_mode = 'HTML')
 
+@dp.message_handler(commands=['help'])
+async def start(message: types.Message):
+    await message.reply('Пока я умею определять пиво только по одному фото, так что пожалуйста, не присылайте мне больше одной фотографии за раз. Желательно, чтобы бутылка или банка занимали большую часть кадра. Мой сервер не самый дорогой, поэтому на ответ мне нужно в среднем 10 секунд.')
+
+@dp.message_handler()
+async def start(message: types.Message):
+    await message.reply('Я умею гадать только по фото')
 
 if __name__ == '__main__': #всегда тру
     executor.start_polling(dp)
